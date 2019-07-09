f4cea85274c5bf34f711bb13f07071907d6302f8
Apple <apple@Apples-MBP-30.attlocal.net>
2019-07-09 10:44:35 -0700

    Added variable

--- practiceGit/ViewController.swift
+++ practiceGit/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var anything = "rishika"
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
